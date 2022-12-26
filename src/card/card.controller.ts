1eae0e7683ac5148019825f798572b9fa4b9c964
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-12-26 18:18:59 -0300

    feat: add controller endpoints

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
+import { CardService } from './card.service';
 
 @Controller('cards')
-export class CardController {}
+export class CardController {
+
+	constructor(private cardService: CardService) {}
+
+	@Get()
+	async findMany(@Query('set-id') setId?: string) {
+		return this.cardService.findMany(setId)
+	}
+
+}
--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CardService {}
+export class CardService {
+
+	constructor(private prisma: PrismaService) {}
+
+	async findMany(setId: string) {
+		const cards = await this.prisma.card.findMany({
+			where: {
+				set_id: setId
+			},
+			take: 1000
+		})
+
+		cards.sort((a, b) => +a.collector_number.replace(/[\D]/g, '') - +b.collector_number.replace(/[\D]/g, ''))
+
+		return cards
+	}
+
+}
