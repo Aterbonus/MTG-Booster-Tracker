6af71e46389a5e463cfc49be6e86bd725dbef208
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2022-12-26 16:48:45 -0300

    feat: seed download bulk card file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,19 @@
 import { PrismaClient } from '@prisma/client'
-import fs from 'fs'
-import bigJson from 'big-json'
+
+const CARD_BATCH_INSERT_SIZE = 10
+
+interface BulkData {
+	id: string
+	uri: string
+	type: string
+	name: string
+	description: string
+	download_uri: string
+	updated_at: string
+	size: number
+	content_type: string
+	content_encoding: string
+}
 
 interface Set {
 	id: string
@@ -23,64 +36,116 @@ interface Card {
 }
 
 const prisma = new PrismaClient()
-const readStream = fs.createReadStream(__dirname + '/cards.json')
-const parseStream = bigJson.createParseStream()
 
-parseStream.on('data', cardsJson => {
-	async function main() {
-		const sets = await fetch('https://api.scryfall.com/sets')
-			.then(data => data.json())
-			.then(({ data }) => data)
+// Get data object from scryfall api
+function fetchToJson<T>(url: string): Promise<T> {
+	return fetch(url)
+	.then(data => data.json())
+	.then(({ data }) => data)
+}
 
-		await prisma.$transaction(async () => {
-			const inserts = []
+function getSets(): Promise<Set[]>{
+	console.log('Getting sets...')
+	return fetchToJson<Set[]>('https://api.scryfall.com/sets').then(sets => {
+		console.log('Sets retrieved...')	
+		return sets
+	})
+}
 
-			for (const set of sets as Set[]) {
-				const [year, month, day] = set.released_at
-					.split('-')
-					.map(n => parseInt(n))
-				const releasedAt = new Date(year, month - 1, day)
+function getBulkCards(): Promise<Card[]> {
+	console.log('Getting cards...')
+	return fetchToJson('https://api.scryfall.com/bulk-data').then((bulks: BulkData[]) => 
+		bulks.find(bulk => bulk.type === 'default_cards')
+	)
+	.then(bulkCards => fetch(bulkCards.download_uri))
+	.then(data => data.json())
+	.then(cards => {
+			console.log('Cards retrieved...')
+			return cards
+	})
+}
 
-				inserts.push(
-					prisma.set.create({
-						data: {
-							id: set.id,
-							code: set.code,
-							name: set.name,
-							icon_svg_uri: set.icon_svg_uri,
-							card_count: set.card_count,
-							released_at: releasedAt
-						}
-					})
-				)
+async function insertSets(sets: Set[]) {
+	
+	console.log('Inserting sets...')
+	
+	await prisma.$transaction(() => {
+		const inserts = []
+
+		for (const set of sets as Set[]) {
+			const [year, month, day] = set.released_at
+				.split('-')
+				.map(n => parseInt(n))
+			const releasedAt = new Date(year, month - 1, day)
+
+			inserts.push(
+				prisma.set.create({
+					data: {
+						id: set.id,
+						code: set.code,
+						name: set.name,
+						icon_svg_uri: set.icon_svg_uri,
+						card_count: set.card_count,
+						released_at: releasedAt
+					}
+				})
+			)
+		}
+
+		return Promise.all(inserts)
+	}, {
+		timeout: 10000
+	})
+
+	console.log('Sets inserted...')
+}
+
+async function insertCards(cards: Card[]) {
+	
+	const inserts = []
+	let count = 0
+
+	console.log(`Inserting cards... ${count}/${cards.length}`)
+
+	for (const card of cards as Card[]) {
+		inserts.push(prisma.card.create({
+			data: {
+				id: card.id,
+				name: card.name,
+				printed_name: card.printed_name,
+				image: card.image_uris?.normal,
+				collector_number: card.collector_number,
+				set_id: card.set_id
 			}
+		}))
 
-			return Promise.all(inserts)
-		})
-
-		for (const card of cardsJson as Card[]) {
-			await prisma.card.create({
-				data: {
-					id: card.id,
-					name: card.name,
-					printed_name: card.printed_name,
-					image: card.image_uris?.normal,
-					collector_number: card.collector_number,
-					set_id: card.set_id
-				}
-			})
+		if (inserts.length === CARD_BATCH_INSERT_SIZE) {
+			await Promise.all(inserts)
+			inserts.length = 0
+			count += CARD_BATCH_INSERT_SIZE
+			console.log(`Inserting cards... ${count}/${cards.length}`)
 		}
 	}
 
-	main()
-		.then(async () => {
-			await prisma.$disconnect()
-		})
-		.catch(async e => {
-			console.error(e)
-			await prisma.$disconnect()
-			process.exit(1)
-		})
-})
+	if (inserts.length) {
+		await Promise.all(inserts)
+	}
 
-readStream.pipe(parseStream)
+	console.log('Cards inserted. All is good :)')
+}
+
+async function main() {
+	
+	const [_, cards] = await Promise.all([getSets().then(insertSets), getBulkCards()])
+	await insertCards(cards)
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
