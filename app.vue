1193c35312546443a92a3fcdf802e0782c7303f2
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2023-01-02 04:23:37 -0300

    feat: theme mode persistence

--- app.vue
+++ app.vue
@@ -1,13 +1,12 @@
 <script lang="ts" setup>
 import '@mdi/font/css/materialdesignicons.css'
-import { useTheme } from 'vuetify/lib/framework.mjs'
 import 'vuetify/styles'
 
 const appName = "Aterbonus' MTG Booster Tracker"
-const theme = useTheme()
+const themeMode = useLocalStorage('theme-mode', 'dark')
 
 function toggleTheme() {
-	theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+	themeMode.value = themeMode.value === 'dark' ? 'light' : 'dark'
 }
 
 useHead({
@@ -17,11 +16,11 @@ useHead({
 </script>
 
 <template>
-	<v-app>
+	<v-app :theme="themeMode">
 		<v-app-bar>
 			<v-app-bar-title>{{ appName }}</v-app-bar-title>
 			<v-btn icon @click="toggleTheme">
-				<v-icon v-if="theme.global.name.value === 'light'">mdi-white-balance-sunny</v-icon>
+				<v-icon v-if="themeMode === 'light'">mdi-white-balance-sunny</v-icon>
 				<v-icon v-else>mdi-weather-night</v-icon>
 			</v-btn>
 			<v-btn icon href="https://github.com/Aterbonus/MTG-Booster-Tracker" target="_blank">
