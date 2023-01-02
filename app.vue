5d8be24c2b8773cc955a5ba567be8f97d7fac9d7
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2023-01-02 04:11:50 -0300

    feat: change theme button

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <script lang="ts" setup>
 import '@mdi/font/css/materialdesignicons.css'
+import { useTheme } from 'vuetify/lib/framework.mjs'
 import 'vuetify/styles'
 
 const appName = "Aterbonus' MTG Booster Tracker"
+const theme = useTheme()
+
+function toggleTheme() {
+	theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
 
 useHead({
 	title: appName,
@@ -14,6 +20,10 @@ useHead({
 	<v-app>
 		<v-app-bar>
 			<v-app-bar-title>{{ appName }}</v-app-bar-title>
+			<v-btn icon @click="toggleTheme">
+				<v-icon v-if="theme.global.name.value === 'light'">mdi-white-balance-sunny</v-icon>
+				<v-icon v-else>mdi-weather-night</v-icon>
+			</v-btn>
 			<v-btn icon href="https://github.com/Aterbonus/MTG-Booster-Tracker" target="_blank">
 				<v-icon>mdi-github</v-icon>
 			</v-btn>
