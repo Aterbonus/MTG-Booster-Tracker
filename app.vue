1ad45da8f6e77209e9dcfffd6e48bb9701f2d4b1
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2023-01-02 00:38:42 -0300

    fix: change app name on footer

--- app.vue
+++ app.vue
@@ -22,7 +22,7 @@ useHead({
 			</ClientOnly>
 		</v-main>
 		<v-footer border app>
-			Aterbonus' MTG Opener is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of
+			Aterbonus' MTG Booster Tracker is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of
 			the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.
 		</v-footer>
 	</v-app>
